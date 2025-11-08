065b7852e39a7c8efb87e7be235cba5f94582dc5
chele-s <elchele0112@gmail.com>
2025-11-08 09:58:18 -0600

    fix

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,5 @@
 from .video_io import VideoReader, VideoWriter
 from .rtmp_client import RTMPClient
-from .config_loader import load_config
+from .config_loader import load_config, merge_configs
 
-__all__ = ['VideoReader', 'VideoWriter', 'RTMPClient', 'load_config']
+__all__ = ['VideoReader', 'VideoWriter', 'RTMPClient', 'load_config', 'merge_configs']
