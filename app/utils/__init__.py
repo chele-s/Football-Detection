f75a21aeb54e5828d246dd12e0c1b084f7b72f1a
chele-s <elchele0112@gmail.com>
2025-11-08 18:01:56 -0600

    ups fix

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,6 @@
 from .video_io import VideoReader, FFMPEGWriter
 from .rtmp_client import RTMPClient
-from .config import load_config, merge_configs
+from .config_loader import load_config, merge_configs
 from .mjpeg_server import MJPEGServer
 
 __all__ = ['VideoReader', 'FFMPEGWriter', 'RTMPClient', 'load_config', 'merge_configs', 'MJPEGServer']
