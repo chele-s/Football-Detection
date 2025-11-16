d00b3027352489e9edfc51db307f943b957df8fe
chele-s <elchele0112@gmail.com>
2025-11-16 17:31:38 -0600

    refactor: add VideoWriter to utils module exports
    - Add VideoWriter import from video_io module
    - Include VideoWriter in __all__ exports for both GPU and non-GPU configurations

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,4 +1,4 @@
-from .video_io import VideoReader, FFMPEGWriter
+from .video_io import VideoReader, VideoWriter, FFMPEGWriter
 from .rtmp_client import RTMPClient
 from .config_loader import load_config, merge_configs
 from .mjpeg_server import MJPEGServer
@@ -7,9 +7,9 @@ from .mjpeg_server import MJPEGServer
 try:
     from .gpu_video_io import GPUVideoReader, GPUVideoWriter, GPUTensorOps
     GPU_AVAILABLE = True
-    __all__ = ['VideoReader', 'FFMPEGWriter', 'RTMPClient', 'load_config', 'merge_configs', 
+    __all__ = ['VideoReader', 'VideoWriter', 'FFMPEGWriter', 'RTMPClient', 'load_config', 'merge_configs', 
                'MJPEGServer', 'GPUVideoReader', 'GPUVideoWriter', 'GPUTensorOps', 'GPU_AVAILABLE']
 except ImportError:
     GPU_AVAILABLE = False
-    __all__ = ['VideoReader', 'FFMPEGWriter', 'RTMPClient', 'load_config', 'merge_configs', 
+    __all__ = ['VideoReader', 'VideoWriter', 'FFMPEGWriter', 'RTMPClient', 'load_config', 'merge_configs', 
                'MJPEGServer', 'GPU_AVAILABLE']
